4ba4052ee62a9c86f425bc96ab6dc76ae4333b3f
Niko Roberts <nikoroberts620@gmail.com>
2019-02-08 17:58:10 -0600

    added api folder, created books.js route file

---
+++ server.js
@@ -0,0 +1,7 @@
+// dependencies
+const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
+
+// import api routes
+const books = require('./routes/api/books');
