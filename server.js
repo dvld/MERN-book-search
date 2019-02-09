c01783d4956f92a4afdc99801f9b6eabc6d71d8d
Niko Roberts <nikoroberts620@gmail.com>
2019-02-08 18:13:24 -0600

    coded server.js

--- server.js
+++ server.js
@@ -5,3 +5,40 @@ const path = require('path');
 
 // import api routes
 const books = require('./routes/api/books');
+
+// instantiate express app
+const app = express();
+
+// set port
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// database config
+const db = require('./config/keys').mongoURI;
+
+// connect to mongo
+mongoose
+  .connect(db, { useNewUrlParser: true })
+  .then(() => console.log('MongoDB Connected...'))
+  .catch(err => console.log(err));
+
+// use routes
+app.use('/api/books', books);
+
+// serve static assets
+if (process.env.NODE_ENV === 'production') {
+  // set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  });
+}
+
+// listener
+app.listen(port, () => {
+  console.log(`Server running on port: ${port}`)
+});
